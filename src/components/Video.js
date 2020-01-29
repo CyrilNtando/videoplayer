b435068edf24302bd60e8fbceb0ead979ba79690
ntando <ntandocyrilnkuna@gmail.com>
2020-01-29 20:54:01 +0200

    added styles and refactor videos

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,5 +1,24 @@
 import React from 'react';
 import ReactPlayer from 'react-player';
-export default function Video() {
-  return <div>Video</div>;
+import StyledVideo from './styles/StyledVideo';
+export default function Video({
+  active,
+  autoplay,
+  endCallBack,
+  progressCallBack
+}) {
+  return (
+    <StyledVideo>
+      <ReactPlayer
+        width='100%'
+        height='100%'
+        style={{ position: 'absolute', top: '0' }}
+        playing={autoplay}
+        controls={true}
+        url={active.video}
+        onEnded={endCallBack}
+        onProgress={progressCallBack}
+      />
+    </StyledVideo>
+  );
 }
